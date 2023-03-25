3e1637a6a02253bc3539de5f8ac89cdae2cf6acd
Tatianam03 <cm0249042022@unad.edu.sv>
2023-03-24 22:29:32 -0600

    DAO

---
+++ Tarea5BasedeDatos/DAO/CrudProductos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea5BasedeDatos.Models;
+
+namespace Tarea5BasedeDatos.DAO
+{
+    public class CrudProductos
+    {
+        public void guardar(Producto paramProducto)
+        {
+            using(AlmacenContext bd = new AlmacenContext())
+            {
+                Producto producto = new Producto();
+                producto.Nombre = paramProducto.Nombre;
+                producto.Descripcion = paramProducto.Descripcion;
+                producto.Precio = paramProducto.Precio;
+                producto.Stock = paramProducto.Stock;
+                bd.Add(producto);
+                bd.SaveChanges();
+                Console.WriteLine("¡Su producto a sido ingresado correctamente!");
+            }
+        }
+    }
+}
---
+++ Tarea5BasedeDatos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Tarea5BasedeDatos;
+using Tarea5BasedeDatos.DAO;
+using Tarea5BasedeDatos.Models;
+
+while (true)
+{
+    Console.WriteLine("Seleccione una opcion del menu");
+    Console.WriteLine("1 - Guardar");
+    Console.WriteLine("2 - Salir");
+    int op = int.Parse(Console.ReadLine());
+
+    switch (op)
+    {
+        case 1:
+            Producto producto = new Producto();
+            CrudProductos CrudProductos = new CrudProductos();
+            Console.WriteLine("Ingrese el nombre del producto");
+            producto.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese la descripcion");
+            producto.Descripcion = Console.ReadLine();
+            Console.WriteLine("Ingrese el precio");
+            producto.Precio = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el Stock");
+            producto.Stock = int.Parse(Console.ReadLine());
+            CrudProductos.guardar(producto);
+            break;
+        case 2:
+            break;
+    }
+    Console.WriteLine("¿Desea volver a ejecutar el programa?");
+    Console.WriteLine("1 - SI");
+    Console.WriteLine("2 - NO");
+    int seguir = int.Parse(Console.ReadLine());
+    if(seguir != 1)
+    {
+        break;
+    }
+
+}
